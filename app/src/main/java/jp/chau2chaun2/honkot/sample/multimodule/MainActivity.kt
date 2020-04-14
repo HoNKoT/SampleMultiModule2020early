d08e0fd874c0aacd27c60481d537a34694ea47ec
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-14 15:19:39 +0900

    migrate to androidx

--- app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/MainActivity.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/MainActivity.kt
@@ -1,6 +1,6 @@
 package jp.chau2chaun2.honkot.sample.multimodule
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
