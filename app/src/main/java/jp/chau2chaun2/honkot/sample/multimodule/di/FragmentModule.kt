fd202f345ab95d59bc87d66391c3fd283ffbed65
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-14 16:04:49 +0900

    add di

--- app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/di/FragmentModule.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/di/FragmentModule.kt
@@ -1,10 +1,17 @@
 package jp.chau2chaun2.honkot.sample.multimodule.di
 
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import jp.chau2chaun2.honkot.sample.multimodule.ui.ListFragment
+import jp.chau2chaun2.honkot.sample.multimodule.ui.SelectFragment
 
 @Module
 abstract class FragmentModule {
 
-//    @ContributesAndroidInjector
-//    abstract fun contributeFirstFragment(): FirstFragment
+    @ContributesAndroidInjector
+    abstract fun contributeSelectFragment(): SelectFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeListFragment(): ListFragment
+
 }
