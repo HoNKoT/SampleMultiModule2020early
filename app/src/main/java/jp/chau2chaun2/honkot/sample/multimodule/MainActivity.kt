9075078fd81ceac3ad9de353dafce2d9a8a373ba
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-14 16:03:53 +0900

    add navigation

--- app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/MainActivity.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/sample/multimodule/MainActivity.kt
@@ -1,12 +1,27 @@
 package jp.chau2chaun2.honkot.sample.multimodule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
